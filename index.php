56efb710c86d6dba382afa8fccb094aecd31c892
John Loudon <jsl245@nau.edu>
2016-05-17 20:44:49 -0700

    added an index.php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<html>
+<head>
+	<title>Readers Indigestion</title>
+</head>
+<body>
+	<h1>It works!</h1>
+	<p>This is the initial web page for this server</p>
+	<p>The web server software is running but no content has been added. yet</p>
+</body>
+</html>
